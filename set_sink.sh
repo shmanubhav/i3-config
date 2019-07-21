e99c2f75cd91bd4993d4d0ea469cd939f2134164
Anubhav Sharma <anubhav@erponweb.com>
2019-07-21 10:56:21 -0400

    Desktop config

--- set_sink.sh
+++ set_sink.sh
@@ -2,4 +2,4 @@
 
 sink_number=$( pactl list sinks | grep -i -B 2 'Name: bluez_sink' | head -n 1 | awk '{ print $2 }' | tail -c 2 )
 echo $sink_number
-sed -i "s/volume ./volume $sink_number/g" ~/.config/i3/config
+sed -i "s/volume . /volume $sink_number /g" ~/.config/i3/config
